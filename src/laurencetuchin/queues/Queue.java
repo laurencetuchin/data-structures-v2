596cf8bf255956396f1010278f3f1ddcf29f3be4
laurencetuchin <laurencetuchin@gmail.com>
2022-05-04 17:43:05 +1000

    Setup Queue and QueueNode

--- src/laurencetuchin/queues/Queue.java
+++ src/laurencetuchin/queues/Queue.java
@@ -1,7 +1,20 @@
 package laurencetuchin.queues;
 
-public class Queue {
-    public static void main(String[] args) {
+public class Queue<T> {
+    private static class QueueNode<T> {
+        private T data;
+        private QueueNode<T> next;
+
+        public QueueNode(T data) {
+            this.data = data;
+        }
 
     }
+
+    private QueueNode<T> first;
+    private QueueNode<T> last;
+
+
+
+
 }
